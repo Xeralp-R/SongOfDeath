ebb89af789b78ad94891e1f3a7b18acb6421e244
Xer-Alp <80310657+Xeralp-R@users.noreply.github.com>
2023-03-06 08:28:48 +0800

    Initial commit

---
+++ desktop/src/ph11/songofdeath/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package ph11.songofdeath;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import ph11.songofdeath.SongOfDeath;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Song of Death");
+		new Lwjgl3Application(new SongOfDeath(), config);
+	}
+}
