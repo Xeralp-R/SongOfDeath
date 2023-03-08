8b911e55de45c3996c2ead090e7b302460b3a41f
Xer-Alp <80310657+Xeralp-R@users.noreply.github.com>
2023-03-08 19:00:26 +0800

    Directly adapted main file
    took in the tmx file that rj made

--- core/src/ph11/songofdeath/SongOfDeath.java
+++ core/src/ph11/songofdeath/SongOfDeath.java
@@ -1,31 +1,70 @@
+/**
+ * The main file, that controls which screen you go to
+ * and what you actually see.
+ *
+ * Lightly (or heavily) adapted from Hugo Descottes
+ * GdxGame
+ */
+
 package ph11.songofdeath;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.gdx.game.manager.PreferenceManager;
+import com.gdx.game.manager.ResourceManager;
+import com.gdx.game.screen.CharacterSelectionScreen;
+import com.gdx.game.screen.GameScreen;
+import com.gdx.game.screen.MenuScreen;
 
-public class SongOfDeath extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
+public class GdxGame extends Game {
+	private SpriteBatch batch;
+	private ResourceManager resourceManager;
+	private PreferenceManager preferenceManager = new PreferenceManager();
+	private MenuScreen menuScreen;
+	private CharacterSelectionScreen characterSelectionScreen;
+	private GameScreen gameScreen;
+
+	public SpriteBatch getBatch() {
+		return batch;
+	}
+
+	public MenuScreen getMenuScreen() {
+		return menuScreen;
+	}
+
+	public CharacterSelectionScreen getCharacterSelectionScreen() {
+		return characterSelectionScreen;
+	}
+
+	public GameScreen getGameScreen() {
+		return gameScreen;
+	}
+
+	public void setGameScreen(GameScreen gameScreen) {
+		this.gameScreen = gameScreen;
+	}
+
+	public PreferenceManager getPreferenceManager() {
+		return preferenceManager;
+	}
+
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		resourceManager = new ResourceManager();
+
+		menuScreen = new MenuScreen(this, resourceManager);
+		characterSelectionScreen = new CharacterSelectionScreen(this, resourceManager);
+
+		this.setScreen(menuScreen);
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
+	public void dispose() {
+		super.dispose();
 		batch.dispose();
-		img.dispose();
+		menuScreen.dispose();
+		characterSelectionScreen.dispose();
+		gameScreen.dispose();
+		resourceManager.dispose();
 	}
 }
