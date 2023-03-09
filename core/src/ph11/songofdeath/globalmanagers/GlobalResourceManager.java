d1428c80c6cad2d1b7089f82f04e4d20bd2ea6b8
Xer-Alp <80310657+Xeralp-R@users.noreply.github.com>
2023-03-09 17:16:07 +0800

    Added texture atlases to GlobalResourceManager.java
    Necessitated a lot of work & coordination. Anyways, the rest should fall faster now,
    but that's fiendishly unlikely.

--- core/src/ph11/songofdeath/globalmanagers/GlobalResourceManager.java
+++ core/src/ph11/songofdeath/globalmanagers/GlobalResourceManager.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.utils.ObjectMap;
 
@@ -21,11 +23,17 @@ public class GlobalResourceManager {
         return instance;
     }
 
+    // Atlases!
+    public final TextureAtlas laMereDeLaTerreAtlas;
+    public final TextureAtlas leDieuDeLaMerAtlas;
+
     // IMAGES
+    // image from https://imgur.com/9dn0cNv
     public Texture background;
 
     // FONT
     public final FreeTypeFontGenerator titleFontGenerator;
+    public final BitmapFont titleFont18;
 
     // Color!
     public final ObjectMap<String, Color> colorScheme;
@@ -33,18 +41,39 @@ public class GlobalResourceManager {
     private static final AssetManager assetManager = new AssetManager();
 
     private GlobalResourceManager() {
+        // Atlas!
+        assetManager.load("pixthulu-ui/pixthulu-ui.atlas", TextureAtlas.class);
+        assetManager.load("terra-mother-ui/terra-mother-ui.atlas", TextureAtlas.class);
+
+        // that one image!
+        assetManager.load("images/graymountain.png", Texture.class);
+
+        assetManager.finishLoading();
+
+        leDieuDeLaMerAtlas = assetManager.get("pixthulu-ui/pixthulu-ui.atlas", TextureAtlas.class);
+        laMereDeLaTerreAtlas = assetManager.get("terra-mother-ui/terra-mother-ui.atlas", TextureAtlas.class);
+
+        this.background = assetManager.get("images/graymountain.png", Texture.class);
+
         // FONT
         this.titleFontGenerator = new FreeTypeFontGenerator(new FileHandle("fonts/AncientModernTales.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 18;
+        this.titleFont18 = this.titleFontGenerator.generateFont(parameter);
 
         // Color
         colorScheme = new ObjectMap<>();
         colorScheme.put("foreground", new Color(1, 1, 1, 1));
         colorScheme.put("background", new Color(1, 0, 0, 0));
-        colorScheme.put("highlightBackground", new Color(1, 0.25F, 0.25F, 0.25F));
+        colorScheme.put("buttonNormalBackground", new Color(1, 0.25F, 0.25F, 0.25F));
+        colorScheme.put("buttonPressedBackground", new Color(1, 0.15F, 0.15F, 0.15F));
     }
 
     public void dispose() {
         assetManager.dispose();
+
+        laMereDeLaTerreAtlas.dispose();
+        leDieuDeLaMerAtlas.dispose();
 
         titleFontGenerator.dispose();
     }
