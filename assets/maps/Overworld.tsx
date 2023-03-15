df8cd75fdbe075db2d3c9a771ca45762665558b0
RJ Marual <b2025rmmarual@pshs.edu.ph>
2023-03-15 23:04:43 +0800

    Added Functional Maps

---
+++ assets/maps/Overworld.tsx
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Overworld" tilewidth="64" tileheight="64" tilecount="27" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="49">
+  <image width="64" height="64" source="x64/Flower1-1.png-1.png (1).png"/>
+ </tile>
+ <tile id="50">
+  <image width="64" height="64" source="x64/Flower2-1.png-1.png (1).png"/>
+ </tile>
+ <tile id="51">
+  <image width="64" height="64" source="x64/Flower3-1.png-1.png (1).png"/>
+ </tile>
+ <tile id="52">
+  <image width="64" height="64" source="x64/Flower4-1.png-1.png.png"/>
+ </tile>
+ <tile id="26">
+  <image width="64" height="64" source="x64/Grass Type1-1.png-1.png.png"/>
+ </tile>
+ <tile id="27">
+  <image width="64" height="64" source="x64/Grass Type2-1.png-1.png.png"/>
+ </tile>
+ <tile id="28">
+  <image width="64" height="64" source="x64/Grass Type3-1.png-1.png.png"/>
+ </tile>
+ <tile id="30">
+  <image width="64" height="64" source="x64/Pathway Corner BR-1.png-1.png.png"/>
+ </tile>
+ <tile id="31">
+  <image width="64" height="64" source="x64/Pathway Corner TL-1.png-1.png.png"/>
+ </tile>
+ <tile id="32">
+  <image width="64" height="64" source="x64/Pathway Corner TR-1.png-1.png.png"/>
+ </tile>
+ <tile id="54">
+  <image width="64" height="64" source="x64/Pathway Corner BL-1.png-1.png.png"/>
+ </tile>
+ <tile id="35">
+  <image width="64" height="64" source="x64/Pathway Corner2 TL-1.png-1.png (1).png"/>
+ </tile>
+ <tile id="34">
+  <image width="64" height="64" source="x64/Pathway Corner2 BR-1.png-1.png.png"/>
+ </tile>
+ <tile id="33">
+  <image width="64" height="64" source="x64/Pathway Corner2 BL-1.png-1.png.png"/>
+ </tile>
+ <tile id="36">
+  <image width="64" height="64" source="x64/Pathway Corner2 TR-1.png-1.png.png"/>
+ </tile>
+ <tile id="37">
+  <image width="64" height="64" source="x64/Pathway Horizontal Down-1.png-1.png (1).png"/>
+ </tile>
+ <tile id="38">
+  <image width="64" height="64" source="x64/Pathway Horizontal Up-1.png-1.png.png"/>
+ </tile>
+ <tile id="41">
+  <image width="64" height="64" source="x64/Pathway Vertical Right-1.png-1.png.png"/>
+ </tile>
+ <tile id="53">
+  <image width="64" height="64" source="x64/Pathway Vertical Left-1.png-1.png.png"/>
+ </tile>
+ <tile id="39">
+  <image width="64" height="64" source="x64/Pathway Universal-1.png-1.png.png"/>
+ </tile>
+ <tile id="42">
+  <image width="64" height="64" source="x64/Solid Grass-1.png.png"/>
+ </tile>
+ <tile id="44">
+  <image width="64" height="64" source="x64/Temp. Character.png"/>
+ </tile>
+ <tile id="45">
+  <image width="64" height="64" source="x64/Temp. Enemy-1.png.png"/>
+ </tile>
+ <tile id="55">
+  <image width="64" height="64" source="../../../OneDrive/Desktop/Graphics/x64/Cliff Top.png"/>
+ </tile>
+ <tile id="47">
+  <image width="64" height="64" source="x64/Cliff Middle.png"/>
+ </tile>
+ <tile id="48">
+  <image width="64" height="64" source="x64/Cliff Bottom.png"/>
+ </tile>
+ <tile id="56">
+  <image width="64" height="64" source="../../../OneDrive/Desktop/Graphics/x64/X.png"/>
+ </tile>
+</tileset>
---
+++ assets/maps/Tree-1.png-1.png.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Tree-1.png-1.png" tilewidth="64" tileheight="64" tilecount="6" columns="2">
+ <image source="x64/Tree-1.png-1.png.png" width="128" height="192"/>
+</tileset>
